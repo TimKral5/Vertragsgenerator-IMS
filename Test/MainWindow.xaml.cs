bf21a3edf7ecf8c55e33c20e634fc7bac79f32bf
TimKral5 <timkral5@outlook.com>
2022-10-24 16:55:29 +0200

    Projektdateien hinzufügen.

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DNBSoft.WPF;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowResizer wr = new WindowResizer(this);
+            wr.addResizerDown(ResizableBottom);
+            wr.addResizerLeft(ResizableLeft);
+            wr.addResizerRight(ResizableRight);
+            wr.addResizerUp(ResizableTop);
+        }
+
+        #region WindowHandler
+        private void MaximizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+            => Close();
+
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+            => WindowState = WindowState.Minimized;
+
+        private void DragBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+            => DragMove();
+
+
+
+        #endregion
+    }
+}
---
+++ Vertragsgenerator/MsWord/WordGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vertragsgenerator.MsWord
+{
+    internal class WordGenerator
+    {
+    }
+}
